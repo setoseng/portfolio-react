79a90a1ee8e4b86e030c1ff178c8b5527428a9eb
Setya Seng <setoseng@gmail.com>
2023-02-15 00:46:31 -0600

    favicon update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03915557ccf203b8"; }
+/******/ 		__webpack_require__.h = function() { return "1ca026bc5dbf8490"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
